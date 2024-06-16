e9dd7459fab2095d99b8dd6531f8be3619664dbe
NikhilZachariah <nikhilzach5@gmail.com>
2024-06-16 18:30:00 +0000

    commit

---
+++ QA/Learning/August/Week 1/Cypress/cypress/e2e/newtours/pages/page.js
@@ -0,0 +1,11 @@
+// export class LoginPage{
+
+//     username_selector='[name=userName]'
+//     password_selector='[name=password]'
+
+//     login(username,password){
+//         cy.get(this.username_selector).type(username)
+//         cy.get(this.password_selector).type(password) 
+//     }
+
+// }
