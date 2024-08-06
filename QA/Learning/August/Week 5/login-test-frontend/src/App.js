a2f80f590460ade67993d57aae9c299fcc370f2a
NikhilZachariah <nikhilzach5@gmail.com>
2024-08-06 18:30:00 +0000

    commit

---
+++ QA/Learning/August/Week 5/login-test-frontend/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import StartTestButton from './components/StartTestButton';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Login Test Automation</h1>
+            <StartTestButton />
+        </div>
+    );
+}
+
+export default App;
